7e58283513ac7b8598a081812e8da17bca526d07
Suvajit Chakrabarty <abhidec1990@gmail.com>
2017-03-15 02:27:43 -0400

    Changed

--- Start.java
+++ Start.java
@@ -8,7 +8,7 @@ public class Start {
     public static void main(String[] args) {
 
 	// write your code here
-        System.out.println("Hello tryouts");
+        System.out.println("Hello everyone.. Starting in 1..2..3");
 
 
         Scanner sc = new Scanner(System.in);
